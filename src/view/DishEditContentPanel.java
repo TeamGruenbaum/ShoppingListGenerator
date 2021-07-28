b3414aaf6e7cef85e5e82e9d6c0d8e50095eb4cc
Steven Solleder <stevensolleder@MacBook-Pro-15-Zoll-2018.fritz.box>
2021-07-29 01:03:37 +0200

    Do add DishEditContentPanel

---
+++ src/view/DishEditContentPanel.java
@@ -0,0 +1,82 @@
+package view;
+
+import model.Dish;
+import model.Ingredient;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class DishEditContentPanel extends JPanel
+{
+    private JTextField name;
+    private JList<Ingredient> ingredients;
+
+    private Dish dish;
+
+
+
+    public <T extends ListModel<Ingredient>> DishEditContentPanel(Dish dish, T listModel)
+    {
+        this.dish=dish;
+
+
+        setLayout(new BorderLayout());
+
+
+        this.name=new JTextField(dish.getName());
+        add(this.name, BorderLayout.NORTH);
+
+
+        ingredients =new JList<>();
+        ingredients.setModel(listModel);
+        ingredients.setSelectionModel(new DefaultListSelectionModel()
+        {
+            @Override
+            public void setSelectionInterval(int index0, int index1)
+            {
+                if(super.isSelectedIndex(index0))
+                {
+                    super.removeSelectionInterval(index0, index1);
+                }
+                else
+                {
+                    super.addSelectionInterval(index0, index1);
+                }
+            }
+        });
+        setSelectedIngredients();
+
+
+        add(new JScrollPane(this.ingredients), BorderLayout.CENTER);
+    }
+
+
+    public void updateDish()
+    {
+        dish.setName(name.getText());
+        dish.getIngredients().clear();
+        dish.getIngredients().addAll(ingredients.getSelectedValuesList());
+    }
+
+
+    private void setSelectedIngredients()
+    {
+        List<Integer> indices=new ArrayList<>();
+        for (Ingredient ingredient:dish.getIngredients())
+        {
+            for (int j = 0; j < ingredients.getModel().getSize(); j++)
+            {
+                if(ingredient== ingredients.getModel().getElementAt(j))
+                {
+                    indices.add(j);
+                    break;
+                }
+            }
+        }
+        ingredients.setSelectedIndices(indices.stream().mapToInt(i -> i).toArray());
+    }
+}
