9451fc456dfb84a5859fc7cc3716e41901c355a4
Steven Solleder <stevensolleder@MacBookPro15Zoll2018.fritz.box>
2021-07-29 12:37:08 +0200

    Change interface definition of DataSaver

--- src/controller/DataSaver.java
+++ src/controller/DataSaver.java
@@ -1,9 +1,10 @@
 package controller;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 public interface DataSaver
 {
-    <T extends Serializable> void write(T object, String target);
-    <T extends Serializable> T read(String source);
+    <T extends Serializable> void write(T object, String target) throws IOException;
+    <T extends Serializable> T read(String source) throws IOException, ClassNotFoundException;
 }
