4ca5abd148993d1a1503caaacd94c1fc61d4b920
isabellwaas <isabell.waas@hof-university.de>
2021-07-29 16:37:39 +0200

    Do implement Pair

---
+++ src/controller/Pair.java
@@ -0,0 +1,38 @@
+package controller;
+
+public class Pair<K,V> implements PairedValue<K,V>
+{
+    private K key;
+    private V value;
+
+
+    public Pair(K key, V value)
+    {
+        this.key=key;
+        this.value=value;
+    }
+
+    @Override
+    public K getKey()
+    {
+        return key;
+    }
+
+    @Override
+    public void setKey(K key)
+    {
+        this.key=key;
+    }
+
+    @Override
+    public V getValue()
+    {
+        return value;
+    }
+
+    @Override
+    public void setValue(V value)
+    {
+        this.value=value;
+    }
+}
