cfedc32193ac7e2fbcccf673733692dc6f6cae54
stevensolleder <stevensolleder@posteo.de>
2021-07-31 18:23:38 +0200

    Do add getter and setter for text and make JTextArea uneditable

--- src/view/TextContentPanel.java
+++ src/view/TextContentPanel.java
@@ -8,10 +8,23 @@ public class TextContentPanel extends JPanel
     private JTextArea text;
 
 
-    public TextContentPanel(String text)
+    public TextContentPanel()
     {
-        this.text=new JTextArea(text);
+        this.text=new JTextArea();
+        this.text.setEditable(false);
+        this.text.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         setLayout(new BorderLayout());
         add(this.text, BorderLayout.CENTER);
+    }
+
+
+    public String getText()
+    {
+        return this.text.getText();
+    }
+
+    public void setText(String text)
+    {
+        this.text.setText(text);
     }
 }
