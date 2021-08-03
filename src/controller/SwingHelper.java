b0d63aefdf319f4fa792644ecf8819226e25c7a4
isabellwaas <isabell.waas@hof-university.de>
2021-08-03 11:56:49 +0200

    Do add getLinkedLabel method in SwingHelper

--- src/controller/SwingHelper.java
+++ src/controller/SwingHelper.java
@@ -1,6 +1,12 @@
 package controller;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class SwingHelper
 {
@@ -17,5 +23,36 @@ public class SwingHelper
         gridBagConstraints.fill = columnFill;
 
         return gridBagConstraints;
+    }
+
+    public static JLabel getLinkedJLabel(String text, String uri)
+    {
+        JLabel linkedLabel=new JLabel(text);
+        linkedLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        linkedLabel.setBorder(BorderFactory.createEmptyBorder(5,0,5,0));
+        linkedLabel.addMouseListener(new MouseListener()
+        {
+            @Override
+            public void mouseClicked(MouseEvent event)
+            {
+                try
+                {
+                    Desktop.getDesktop().browse(new URI(uri));
+                }
+                catch(IOException | URISyntaxException exception)
+                {
+                    JOptionPane.showMessageDialog(new JFrame(), "The URI could not be opened.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+            @Override
+            public void mousePressed(MouseEvent e){}
+            @Override
+            public void mouseReleased(MouseEvent e){}
+            @Override
+            public void mouseEntered(MouseEvent e){}
+            @Override
+            public void mouseExited(MouseEvent e){}
+        });
+        return linkedLabel;
     }
 }
