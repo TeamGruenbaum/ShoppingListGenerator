956084513c321d3650a987ba5a9aabbdf8cbed6b
isabellwaas <isabell.waas@hof-university.de>
2021-08-04 20:18:50 +0200

    Do optimize Localisator

--- src/controller/Localisator.java
+++ src/controller/Localisator.java
@@ -1,33 +1,23 @@
 package controller;
 
+
 import java.util.ResourceBundle;
+
+
 
 public class Localisator
 {
-    private static Localisator instance=null;
-
     private ResourceBundle resourceBundle;
 
 
-
-    private Localisator()
+    public Localisator()
     {
         this.resourceBundle = ResourceBundle.getBundle("resources.strings");
     }
 
+
     public String getString(String key)
     {
         return resourceBundle.getString(key);
-    }
-
-
-    public static Localisator getInstance()
-    {
-        if(instance==null)
-        {
-            instance=new Localisator();
-        }
-
-        return instance;
     }
 }
